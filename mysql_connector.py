6a98235b6c90e39f217846dd2878fcca2516f8f8
Vignesh R <43677580+vigneshr1603@users.noreply.github.com>
2021-05-02 11:34:22 +0530

    changes

--- mysql_connector.py
+++ mysql_connector.py
@@ -5,7 +5,7 @@ import mysql.connector;
 mydb = mysql.connector.connect(
     host="localhost",
     user="password",     # <-- change the username here
-    password="vikki2001",   # <---- change the password here
+    password="password",   # <---- change the password here
     database="train_booking_application"
 
 )
